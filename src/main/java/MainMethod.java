e537e51786c517eb7a896c4e5c4a158e1209f4de
Oleh <olehfromukraine@gmail.com>
2020-08-21 00:04:07 +0300

    Try done homework 1.1

---
+++ src/main/java/MainMethod.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class MainMethod {
+    public static void main(String[] args) {
+        for(int a =0 ; a <args.length; a++){
+            System.out.print(args[a]+ " ");
+        }
+    }
+}
